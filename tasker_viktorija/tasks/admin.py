b1d09a1ae35f7235db54db4a964398ce64acc6c4
victoria985 <viktorijashot@gmail.com>
2024-02-13 17:57:38 +0200

    Django 0202,0301

--- tasker_viktorija/tasks/admin.py
+++ tasker_viktorija/tasks/admin.py
@@ -3,75 +3,60 @@ from django.utils.translation import gettext_lazy as _
 from . import models
 
 
-class ProdjectAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'total_tasks', 'undone_tasks', 'owner', 'recent_tasks']
-    list_display_links = ['id', 'name']
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['name', 'total_tasks', 'undone_tasks', 'owner', 'recent_tasks']
+    list_display_links = ['name']
     list_filter = ['owner']
     search_fields = ['name']
     fieldsets = (
         (None, {
             "fields": (
-                ('name', 'owner')
+                'name', 'owner',
             ),
         }),
     )
 
-    def total_tasks(self, objektas: models.Project):
-        return objektas.tasks_count()
-    total_tasks.short_description =_("total tasks")
+    def total_tasks(self, obj: models.Project):
+        return obj.tasks.count()
+    total_tasks.short_description = _("total tasks")
 
-    def undone_tasks(self, objektas: models.Project):
-        return objektas.tasks.filter(is_done=False).count()
-    undone_tasks.short_description =_("undone tasks")
-    
-    def recent_tasks(self, objektas: models.Project):
-        return "; ".join(objektas.tasks_order_by('-created_at').values_list('name', flat=True)[:3])
-    recent_tasks.short_description = _("recent tasks")    
+    def undone_tasks(self, obj: models.Project):
+        return obj.tasks.filter(is_done=False).count()
+    undone_tasks.short_description = _("undone tasks")
+
+    def recent_tasks(self, obj: models.Project):
+        return "; ".join(obj.tasks.order_by('-created_at').values_list('name', flat=True)[:3])
+    recent_tasks.short_description = _("recent tasks")
 
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['name', 'is_done', 'deadline', 'project', 'owner', 'created_at']
     list_filter = ['is_done', 'deadline', 'created_at']
-    search_fields = ['name', 'description', 'project__name', 'owner__lasst_name']    
+    search_fields = ['name', 'description', 'project__name', 'owner__last_name', 'owner__username']
     list_editable = ['is_done', 'owner', 'project']
     readonly_fields = ['id', 'created_at', 'updated_at']
     fieldsets = (
-        (("general").title(), {
+        (_("general").title(), {
             "fields": (
                 ('name', 'deadline'),
                 'description', 'is_done',
-                
             ),
         }),
-    
-    
-        (("ownership").title(), {
+        (_("ownership").title(), {
             "fields": (
                 ('owner', 'project'),
-                
             ),
         }),
-    
-    
-        (("temporal tracking").title(), {
+        (_("temporal tracking").title(), {
             "fields": (
                 ('created_at', 'updated_at', 'id'),
-                
             ),
         }),
     )
     
 
 
-
-
-
-
-
-
-
-
-
-admin.site.register(models.Project, ProdjectAdmin)
+admin.site.register(models.Project, ProjectAdmin)
 admin.site.register(models.Task, TaskAdmin)
+
 
--- tasker_viktorija/tasks/views.py
+++ tasker_viktorija/tasks/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        'procects_count': models.Project.objects.count(),
+        'tasks_count': models.Task.objects.count(),
+        'users_count': models.get_user_model().objects.count(),    
+    }
+    return render(request, 'tasks/index.html', context)
+
