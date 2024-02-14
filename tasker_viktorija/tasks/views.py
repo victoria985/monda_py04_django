8a61269b3b7035d19b1576b9509858040c2660e6
victoria985 <viktorijashot@gmail.com>
2024-02-14 19:01:40 +0200

    static-css atkartojimas pagal dest. pvz.

--- tasker_viktorija/tasks/views.py
+++ tasker_viktorija/tasks/views.py
@@ -1,12 +1,13 @@
+from django.contrib import messages
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from . import models
 
 def index(request: HttpRequest) -> HttpResponse:
     context = {
-        'procects_count': models.Project.objects.count(),
+        'projects_count': models.Project.objects.count(),
         'tasks_count': models.Task.objects.count(),
-        'users_count': models.get_user_model().objects.count(),    
+        'users_count': models.get_user_model().objects.count(),
     }
     return render(request, 'tasks/index.html', context)
 
@@ -19,4 +20,13 @@ def task_detail(request: HttpRequest, pk: int) -> HttpResponse:
     return render(request, 'tasks/task_detail.html', {
         'task': get_object_or_404(models.Task, pk=pk),
     })
+
+def task_done(request: HttpRequest, pk: int) -> HttpResponse:
+    task = get_object_or_404(models.Task, pk=pk)
+    task.is_done = not task.is_done
+    task.save()
+    messages.success(request, f"Task {task.name} marked as {'done' if task.is_done else 'undone'}.")
+    if request.GET.get('next'):
+        return redirect(request.GET.get('next'))
+    return redirect(task_list)
 
