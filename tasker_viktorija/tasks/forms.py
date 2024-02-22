e93d89c5655a4570bacebaf5a06b4097d97c2ed1
victoria985 <viktorijashot@gmail.com>
2024-02-22 11:02:38 +0200

    index, dashboardo style sukurimas

--- tasker_viktorija/tasks/forms.py
+++ tasker_viktorija/tasks/forms.py
@@ -2,8 +2,15 @@ from django import forms
 from . import models
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = models.Task
         fields = ('name', 'project', 'description', 'deadline', 'is_done', )
+        widgets = {
+            'deadline': DateInput,
+        }
         
